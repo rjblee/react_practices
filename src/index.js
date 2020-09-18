0b29fadeba64a1201450a297d3c8be4b98052b0e
Ralph Lee <50860714+rjblee@users.noreply.github.com>
2020-09-17 17:57:41 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite Foods
+    </h1>
+    <div>
+      <img
+        src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/8/26/DV3013_steak_shawarma-pita_s4x3.JPG.rend.hgtvcom.826.620.suffix/1566851027046.jpeg"
+        width="100px"
+        height="100px"
+      />
+      <img
+        src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/8/26/DV3013_steak_shawarma-pita_s4x3.JPG.rend.hgtvcom.826.620.suffix/1566851027046.jpeg"
+        width="100px"
+        height="100px"
+      />
+      <img
+        src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/8/26/DV3013_steak_shawarma-pita_s4x3.JPG.rend.hgtvcom.826.620.suffix/1566851027046.jpeg"
+        width="100px"
+        height="100px"
+      />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
